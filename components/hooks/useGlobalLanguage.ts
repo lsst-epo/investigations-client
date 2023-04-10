867c98a927290ea4f0c89e5c0fb5c98d5af49542
Alexandra Goff <agoff@lsst.org>
2023-04-10 13:24:47 -0700

    [C] remove stuff accidentally copied to components

--- components/hooks/useGlobalLanguage.ts
+++
@@ -1,8 +0,0 @@
-import { useGlobalData } from "@/hooks";
-
-const useGlobalLanguage = () => {
-  const { language } = useGlobalData("localeInfo");
-  return language;
-};
-
-export default useGlobalLanguage;
--- components/lib/localeStrings/index.ts
+++
@@ -1,9 +0,0 @@
-import merge from "lodash/merge";
-import { localeStrings } from "@rubin-epo/epo-react-lib";
-import en from "./en.json";
-import es from "./es.json";
-
-export default {
-  en: merge(en, localeStrings.en),
-  es: merge(es, localeStrings.es),
-};
