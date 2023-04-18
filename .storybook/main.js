8a37f49102c334be8b749d5e491bd8347d60281f
Alexandra Goff <agoff@lsst.org>
2023-04-18 16:52:42 -0700

    [F] set up TabbedSelector

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-react-i18next",
   ],
   framework: {
     name: "@storybook/nextjs",
