4ab9aad90cf38d30b9485c5726ea8fa6f77c7bd3
Alexandra Goff <agoff@lsst.org>
2023-04-18 16:52:42 -0700

    [F] set up TabbedSelector

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,19 @@
 import "@/styles/styles.scss";
 import globalDecorators from "./decorators";
+import i18n from "./i18next.js";
+
+const preview = {
+  globals: {
+    locale: "en",
+    locales: {
+      en: "English",
+      es: "Español",
+    },
+  },
+  parameters: {
+    i18n,
+  },
+};
+export default preview;
 
 export const decorators = globalDecorators;
