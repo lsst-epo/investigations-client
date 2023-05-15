1bfb53422b1cb5949f7dad12cc20d6e18765a29f
Alexandra Goff <agoff@lsst.org>
2023-05-15 12:57:45 -0700

    [C] try loading file

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const RootLoading: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Loading</h1>
+    </div>
+  );
+};
+
+export default RootLoading;
