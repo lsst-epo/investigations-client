5e70445aa61071011813b558f7687d28b43f35e0
Alexandra Goff <agoff@lsst.org>
2023-05-25 14:42:26 -0700

    [F] app directory setup

--- components/templates/Page/index.tsx
+++ components/templates/Page/index.tsx
@@ -1,9 +1,9 @@
+"use client";
 import { FunctionComponent, ReactNode } from "react";
 import PageData from "@/shapes/page";
 import Body from "@/global/Body";
 import ContentBlockFactory from "@/components/factories/ContentBlockFactory";
 import { Container } from "@rubin-epo/epo-react-lib";
-import Header from "@/components/global/Header";
 
 interface PageProps {
   data: PageData;
@@ -13,13 +13,9 @@ interface PageProps {
 const Page: FunctionComponent<PageProps> = ({
   data: { id, title, contentBlocks = [], uri },
 }) => {
-  const bodyProps = {
-    title,
-  };
   return (
-    <Body {...bodyProps}>
+    <Body>
       <Container>
-        <Header />
         <h1>{title}</h1>
         {[...contentBlocks].map((block) => {
           if (!block.id || !block.typeHandle) return null;
