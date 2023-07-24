26f868bf516173d7a4c1f5876a9c8a189e865eb7
Alexandra Goff <agoff@lsst.org>
2023-07-24 16:59:08 -0700

    [F] tabular question and demo

--- components/questions/TabularQuestion/Text/styles.ts
+++ components/questions/TabularQuestion/Text/styles.ts
@@ -1,11 +1,6 @@
 import styled from "styled-components";
 import { TextInput as BaseTextInput } from "../../SimpleQuestion/Text/styles";
 
-export const InputContainer = styled.div`
-  display: flex;
-  gap: 1em;
-`;
-
 export const TextInput = styled(BaseTextInput)`
   --text-bg: var(--white, #fff);
 
