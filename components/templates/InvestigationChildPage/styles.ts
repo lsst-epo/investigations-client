280a41e58d5cf0b6e46e60652bdbe9bd9af3373b
Blake Mason <bmason@lsst.org>
2023-09-05 17:01:12 -0700

    [F] ChildPage Container padding based on Pager height

--- components/templates/InvestigationChildPage/styles.ts
+++ components/templates/InvestigationChildPage/styles.ts
@@ -1,2 +1,7 @@
 import styled from "styled-components";
 import Container from "@rubin-epo/epo-react-lib/Container";
+
+export const ContentBlocks = styled(Container)`
+  padding-block-start: 67px;
+  padding-block-end: var(--pager-height);
+`;
