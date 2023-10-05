dc8b700dd350e3ae971e3956816a282067045ffc
Blake Mason <bmason@lsst.org>
2023-10-05 16:12:02 -0700

    [F] Question numbering is over whole investigation

--- components/content-blocks/Questions/index.tsx
+++ components/content-blocks/Questions/index.tsx
@@ -1,6 +1,8 @@
+import { useContext } from "react";
 import { I18nextProvider } from "react-i18next";
 import { useTranslation } from "@/lib/i18n/client";
 import { graphql, useFragment, FragmentType } from "@/gql/public-schema";
+import ProgressContext from "@/contexts/Progress";
 import QuestionFactory from "@/components/factories/QuestionFactory";
 import * as Styled from "./styles";
 
@@ -23,11 +25,12 @@ export default function QuestionsContentBlock(props: {
   const data = useFragment(Fragment, props.data);
 
   const { i18n, t } = useTranslation();
+  const { questions } = useContext(ProgressContext);
 
   return (
     <I18nextProvider i18n={i18n}>
       <section className="content-block">
-        <Styled.Heading>{t('page.questions')}</Styled.Heading>
+        <Styled.Heading>{t("page.questions")}</Styled.Heading>
         <Styled.QuestionList
           style={{
             "--list-background-color": isInteraction && "#E6FFE6",
@@ -35,15 +38,18 @@ export default function QuestionsContentBlock(props: {
           }}
         >
           {!!data.questionEntries?.length &&
-            data.questionEntries.map((question, index) =>
-              question?.__typename === "questions_default_Entry" ? (
+            data.questionEntries.map((question) => {
+              const { id } = question;
+              const questionIndex = questions.indexOf(id);
+
+              return question?.__typename === "questions_default_Entry" ? (
                 <QuestionFactory
                   key={question.id}
                   data={question}
-                  config={{ number: index + 1 }}
+                  config={{ number: questionIndex + 1}}
                 />
-              ) : null
-            )}
+              ) : null;
+            })}
         </Styled.QuestionList>
       </section>
     </I18nextProvider>
