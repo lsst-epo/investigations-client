975994a4296375250d6d87e01d4748cad81794b7
Blake Mason <bmason@lsst.org>
2023-10-05 13:34:13 -0700

    [B] ColorFilter has correct props

--- components/content-blocks/FilterTool/FilterTool.tsx
+++ components/content-blocks/FilterTool/FilterTool.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container } from "@rubin-epo/epo-react-lib";
 import { graphql, useFragment, FragmentType } from "@/gql/public-schema";
 import FilterTool from "@rubin-epo/epo-widget-lib/FilterTool";
@@ -15,11 +16,16 @@ export default function FilterToolBlock(props: {
   data: FragmentType<typeof Fragment>;
 }) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const data = useFragment(Fragment, props.data);
+  const { preSelectedColor, readOnly } = useFragment(Fragment, props.data);
+  const [selectedColor, setSelectedColor] = useState(preSelectedColor);
 
   return (
     <Container>
-      <FilterTool />
+      <FilterTool
+        isDisabled={readOnly}
+        selectedColor={selectedColor}
+        selectionCallback={(selection) => setSelectedColor(selection)}
+      />
     </Container>
   );
 }
