dd7862407585c0a7db5176b97a469efb2da57604
Alexandra Goff <agoff@lsst.org>
2023-11-02 11:19:27 -0700

    [C] add reference modals as a child of containers

--- components/content-blocks/TwoColumnContainer/styles.ts
+++ components/content-blocks/TwoColumnContainer/styles.ts
@@ -3,23 +3,31 @@ import { BREAK_TABLET } from "@/styles/globalStyles";
 
 export const LeftCol = styled.div`
   display: block;
+
+  > * + * {
+    margin-block-start: var(--content-block-margin);
+  }
 `;
 
 export const RightCol = styled.div`
   display: block;
+
+  > * + * {
+    margin-block-start: var(--content-block-margin);
+  }
 `;
 
 export const TwoColContainer = styled.section`
-	display: block;
+  display: block;
 
-	@media (min-width: ${BREAK_TABLET}) {
-		display: flex;
-		justify-content: space-between;
-	}
+  @media (min-width: ${BREAK_TABLET}) {
+    display: flex;
+    justify-content: space-between;
+  }
 
-    ${LeftCol}, ${RightCol} {
-      @media (min-width: ${BREAK_TABLET}) {
-        width: calc(50% - 20px);
-      }
+  ${LeftCol}, ${RightCol} {
+    @media (min-width: ${BREAK_TABLET}) {
+      width: calc(50% - 20px);
     }
+  }
 `;
