96c72c1c8908d9c7d1ca701d5a1408a1dd1f487a
Alexandra Goff <agoff@lsst.org>
2023-11-10 12:18:55 -0700

    [C] final Color Tool cleanup

--- components/questions/SimpleQuestion/Widget/ColorFilterTool/styles.ts
+++ components/questions/SimpleQuestion/Widget/ColorFilterTool/styles.ts
@@ -1,24 +1,21 @@
 import styled from "styled-components";
 import WidgetContainer from "@/components/layout/WidgetContainer";
-import { token } from "@rubin-epo/epo-react-lib/styles";
-
-const breakSize = token("BREAK_LARGE_TABLET_MIN");
 
 export const ColorToolContainer = styled(WidgetContainer)`
   --color-tool-padding: var(--PADDING_SMALL, 20px);
 
   &[data-modal-open="true"] {
-    --color-tool-padding: var(--PADDING_MEDIUM, 40px);
-
     background-color: var(--white, #fff);
     position: absolute;
     height: 100%;
     width: 100%;
     top: 0;
+    overflow-y: auto;
   }
 
-  @container contentModal (min-width: ${breakSize}) {
+  @container contentModal (min-width: 940px) {
     &[data-modal-open="true"] {
+      --color-tool-padding: var(--PADDING_MEDIUM, 40px);
       max-width: 2000px;
       margin-inline: auto;
       position: static;
