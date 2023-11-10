890d5438dbe8d6885fb9a080964cc3570b1160f5
Alexandra Goff <agoff@lsst.org>
2023-11-10 12:18:55 -0700

    [C] final Color Tool cleanup

--- components/content-blocks/ColorFilterTool/styles.ts
+++ components/content-blocks/ColorFilterTool/styles.ts
@@ -9,21 +9,41 @@ export const ColorToolContainer = styled(WidgetContainer)`
 
   &[data-modal-open="true"] {
     --color-tool-padding: var(--PADDING_MEDIUM, 40px);
+    --frame-max-height: calc(100dvh - var(--color-tool-padding) * 2);
+    --frame-ratio-w: 1;
+    --frame-ratio-h: 1;
 
-    background-color: var(--white, #fff);
-    position: absolute;
-    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-inline: auto;
     width: 100%;
-    top: 0;
-  }
+    max-width: 2000px;
+    height: 100%;
 
-  @container contentModal (min-width: ${breakSize}) {
-    &[data-modal-open="true"] {
-      max-width: 2000px;
-      max-height: 100%;
-      margin-inline: auto;
-      position: static;
-      height: auto;
+    > div {
+      /* Padding is width/height, since % padding is based off the element's width */
+      --ratio: calc(var(--frame-ratio-h, 1) / var(--frame-ratio-w, 1) * 100%);
+      /**
+      * The height of the frame is either the calculated padding value, or a maximum
+      * passed in (using --frame-max-height). This effectively clamps the height.
+      */
+      --frame-height: min(var(--ratio), var(--frame-max-height));
+      position: relative;
+      padding-bottom: var(--frame-height);
+      /**
+      * The width should be 100% where possible, but should maintain aspect ratio
+      * first and foremost. In order to do so we can take the calculated height
+      * and reverse engineer the width.
+      */
+      width: min(
+        calc(
+          var(--frame-height, 0) * (var(--frame-ratio-w) / var(--frame-ratio-h))
+        ),
+        100%
+      );
+      height: 0;
     }
   }
 `;
