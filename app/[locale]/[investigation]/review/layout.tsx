0ec4a40ac8f4015a26997ac434735a0500b91f6a
Alexandra Goff <agoff@lsst.org>
2023-11-14 13:37:50 -0700

    [C] wire up header and pager

--- app/[locale]/[investigation]/review/layout.tsx
+++ app/[locale]/[investigation]/review/layout.tsx
@@ -1,6 +1,9 @@
 import { FunctionComponent, PropsWithChildren } from "react";
 import { RootLayoutParams } from "../../layout";
 import { InvestigationParams } from "../layout";
+import { ProgressProvider } from "@/contexts/Progress";
+import Header from "@/page/Header/Header";
+import Pager from "@/page/Pager";
 
 export interface ReviewPageProps {
   params: RootLayoutParams & InvestigationParams;
@@ -9,7 +12,13 @@ export interface ReviewPageProps {
 const ReviewLayout: FunctionComponent<PropsWithChildren<ReviewPageProps>> = ({
   children,
 }) => {
-  return <>{children}</>;
+  return (
+    <ProgressProvider currentPageId="review">
+      <Header />
+      {children}
+      <Pager />
+    </ProgressProvider>
+  );
 };
 
 export default ReviewLayout;
