7b6c3da9afe61a4d2216aa85e99b4fab7979b59d
Alexandra Goff <agoff@lsst.org>
2023-12-01 14:58:41 -0700

    [C] signed in and signed out forms

---
+++ components/auth/investigation/styles.ts
@@ -0,0 +1,12 @@
+"use client";
+import styled from "styled-components";
+import Buttonish from "@rubin-epo/epo-react-lib/Buttonish";
+
+export const WithoutLoginLink = styled(Buttonish)`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LinkLabel = styled.span`
+  font-size: 60%;
+`;
