66823750ce6b6378bd986c93ddb15bb5349eb6d7
Alexandra Goff <agoff@lsst.org>
2023-12-08 16:45:22 -0700

    [C] interaction groups span screen width on mobile

--- components/content-blocks/InteractionGroupContainer/styles.ts
+++ components/content-blocks/InteractionGroupContainer/styles.ts
@@ -1,19 +1,24 @@
 import styled from "styled-components";
-import { fluidScale } from "@rubin-epo/epo-react-lib/styles";
+import { fluidScale, token } from "@rubin-epo/epo-react-lib/styles";
 
 export const Heading = styled.h2`
   margin-block-end: ${fluidScale("2em", "1.5em")};
 `;
 export const InteractionGroup = styled.div`
-  --content-block-margin: ${fluidScale(
-    "var(--PADDING_MEDIUM, 40px)",
-    "var(--PADDING_SMALL, 20px)"
-  )};
+  --interaction-group-margin: var(
+    --content-block-margin,
+    ${fluidScale("var(--PADDING_MEDIUM, 40px)", "var(--PADDING_SMALL, 20px)")}
+  );
 
-  padding: ${fluidScale("2em", "1.5em")};
+  padding: var(--interaction-group-margin);
+  margin-inline: calc(-1 * var(--interaction-group-margin));
   background-color: #e6ffe6;
 
   > * + * {
-    margin-block-start: var(--content-block-margin);
+    margin-block-start: var(--interaction-group-margin);
+  }
+
+  @media screen and (min-width: ${token("BREAK_TABLET_MIN")}) {
+    margin-inline: 0;
   }
 `;
