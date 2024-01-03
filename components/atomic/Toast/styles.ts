e005f65e5b3d6ded6de24c0cef817723c09cc9dd
Alexandra Goff <agoff@lsst.org>
2024-01-03 14:33:41 -0700

    [F] react-toastify setup

---
+++ components/atomic/Toast/styles.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import BaseToast from "@rubin-epo/epo-react-lib/Toast";
+
+export const Toast = styled(BaseToast)`
+  display: flex;
+  background-color: #dce0e3;
+  padding: 1.5em;
+  width: 100%;
+  position: relative;
+  transform: var(--toast-transform);
+  transition: var(--toast-transition, unset);
+`;
+
+export const ToastContent = styled.div`
+  display: flex;
+  gap: 1em;
+  margin-inline: auto;
+  max-width: 70ch;
+  width: 100%;
+`;
+
+export const ToastIcon = styled.div`
+  aspect-ratio: 1;
+  background-color: var(--white, #fff);
+  border: 1px solid var(--border-color);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 2em;
+  stroke-width: 0;
+`;
+
+export const CloseToast = styled.button`
+  background: none;
+  border: 0;
+  margin: 0;
+  padding: 0;
+  color: var(--turquoise85, #12726d);
+  position: absolute;
+  top: 0.5em;
+  right: 0.5em;
+`;
+
+export const ToastText = styled.div`
+  > * + * {
+    margin-top: 1em;
+  }
+`;
