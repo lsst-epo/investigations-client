02919180c38079a66e86644d8511f8223fb4a091
Alexandra Goff <agoff@lsst.org>
2024-01-04 13:15:37 -0700

    [C] switch to react-hot-toast
    For JIRA: "EPO-8937 #IN-REVIEW #comment set up toast"

--- components/layout/Toaster/styles.ts
+++ components/layout/Toaster/styles.ts
@@ -1,42 +1,39 @@
 import styled from "styled-components";
-import BaseToast from "@rubin-epo/epo-react-lib/Toast";
+import { Toaster as BaseToaster } from "react-hot-toast";
 
-export const Toaster = styled.div`
-  --toast-transform: translateY(0);
-
-  margin: 0;
-  position: fixed;
-  width: 100%;
-  bottom: 0;
-  left: 0;
-  transform: translateY(100%);
-  z-index: 1;
-
-  &[data-visible="true"] {
-    --toast-transform: translateY(-100%);
+export const Toaster = styled(BaseToaster)`
+  @keyframes slideIn {
+    from {
+      transform: translateY(100%);
+    }
+    to {
+      transform: translateY(0);
+    }
   }
-
-  @media (prefers-reduced-motion: no-preference) {
-    --toast-transition: transform ease var(--toast-delay);
+  @keyframes slideOut {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(100%);
+    }
   }
 `;
 
-export const Toast = styled(BaseToast)`
-  display: flex;
-  background-color: #dce0e3;
-  padding: 1.5em;
-  width: 100%;
-  position: relative;
-  transform: var(--toast-transform);
-  transition: var(--toast-transition, unset);
-`;
-
-export const ToastContent = styled.div`
+export const ToastContainer = styled.div`
   display: flex;
   gap: 1em;
   margin-inline: auto;
   max-width: 70ch;
   width: 100%;
+
+  & > [role="status"] {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 export const ToastIcon = styled.div`
@@ -60,10 +57,4 @@ export const CloseToast = styled.button`
   position: absolute;
   top: 0.5em;
   right: 0.5em;
-`;
-
-export const ToastText = styled.div`
-  > * + * {
-    margin-top: 1em;
-  }
 `;
