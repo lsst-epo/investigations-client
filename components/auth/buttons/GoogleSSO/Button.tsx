737610affe059ad2051df23ffc58eae63e7d4d80
Blake Mason <bmason@lsst.org>
2024-01-08 16:31:16 -0800

    [B] Google OAuth fix
     - Includes custom Google login button instead of Google's

--- components/auth/buttons/GoogleSSO/Button.tsx
+++ components/auth/buttons/GoogleSSO/Button.tsx
@@ -1,73 +1,59 @@
 "use client";
 
-import type { GoogleCredentialResponse } from "@react-oauth/google";
-import { Button } from "@rubin-epo/epo-react-lib";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useGoogleLogin } from "@react-oauth/google";
+import { Button } from "@rubin-epo/epo-react-lib";
 import { useAuthDialogManager } from "@/contexts/AuthDialogManager";
 import { usePathToRevalidate } from "@/components/auth/clientHelpers";
-import { authenticateEducator, authenticateStudent } from "./actions";
-import { GoogleLogin } from "@react-oauth/google";
-// import { useGoogleLogin } from "@react-oauth/google";
+import { getTokens, authenticateUser } from "./actions";
 
 export default function SSOButton({
   onError: onSignInError,
-  buttonWidth
 }: {
   onError: () => void;
-  buttonWidth: number;
 }) {
-  const {
-    // t,
-    i18n: { language },
-  } = useTranslation();
+  const { t } = useTranslation();
   const { pendingGroup } = useAuthDialogManager();
   const pathToRevalidate = usePathToRevalidate();
-  // const login = useGoogleLogin({
-  //   onSuccess: handleSuccess,
-  //   onError: handleError,
-  // });
+  const login = useGoogleLogin({
+    onSuccess: handleSuccess,
+    onError: handleError,
+    flow: "auth-code",
+  });
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleError() {
-    onSignInError(response);
+    onSignInError();
   }
 
-  async function handleSuccess(credentialResponse: GoogleCredentialResponse) {
-    try {
-      if (!credentialResponse.credential) return handleError();
+  async function handleSuccess(res) {
+    const { code } = res;
+    const { id_token: idToken } = await getTokens(code);
 
-      if (pendingGroup === "educators") {
-        await authenticateEducator(
-          credentialResponse.credential,
-          pathToRevalidate
-        );
-      } else {
-        await authenticateStudent(
-          credentialResponse.credential,
-          pathToRevalidate
-        );
-      }
+    try {
+      if (!idToken) return handleError();
+      await authenticateUser(pendingGroup, idToken, pathToRevalidate);
     } catch (error) {
-      onSignInError();
+      handleError();
+      setIsLoading(false);
     }
   }
 
-  // return (
-  //   <Button onClick={() => login()} styleAs="tertiary">
-  //     {t("sign_in.continue_with_google")}
-  //   </Button>
-  // );
   return (
-    <GoogleLogin
-      onSuccess={handleSuccess}
-      onError={handleError}
-      locale={language}
-      shape="rectangle"
-      size="large"
-      text="continue_with"
-      theme="outline"
-      type="standard"
-      width={buttonWidth}
-    />
+    <Button
+      onClick={() => {
+        setIsLoading(true);
+        login();
+      }}
+      styleAs="tertiary"
+      disabled={isLoading}
+      isBlock
+    >
+      {isLoading
+        ? t("sign_in.loading_google")
+        : t("sign_in.continue_with_google")}
+    </Button>
   );
 }
 
