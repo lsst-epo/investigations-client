1a0fb917f7559f9cf46f07adead15683644fe3c2
Blake Mason <bmason@lsst.org>
2024-01-08 16:31:16 -0800

    [B] Google OAuth fix
     - Includes custom Google login button instead of Google's

--- components/auth/buttons/GoogleSSO/GoogleSSO.tsx
+++ components/auth/buttons/GoogleSSO/GoogleSSO.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { useState } from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import useResizeObserver from "use-resize-observer";
 import Button from "./Button";
 
 const GOOGLE_APP_ID = process.env.NEXT_PUBLIC_GOOGLE_APP_ID;
@@ -14,25 +12,11 @@ export default function GoogleSSO({
   className: string;
   onError: () => void;
 }) {
-  const [buttonWidth, setButtonWidth] = useState();
-
-  const { ref } = useResizeObserver({
-    onResize: ({ width }) => {
-      setButtonWidth(width);
-    },
-  });
-
   if (!GOOGLE_APP_ID) return null;
 
   return (
     <GoogleOAuthProvider clientId={GOOGLE_APP_ID}>
-      <div ref={ref}>
-        <Button
-          className={className}
-          onError={onError}
-          buttonWidth={buttonWidth}
-        />
-      </div>
+      <Button className={className} onError={onError} />
     </GoogleOAuthProvider>
   );
 }
