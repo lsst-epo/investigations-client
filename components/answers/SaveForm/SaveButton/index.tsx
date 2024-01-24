6c43b46d3b371368bed88b4049610edf0097a7bd
Alexandra Goff <agoff@lsst.org>
2024-01-24 11:01:33 -0700

    [F] add login button to menu & save form
    For JIRA: "EPO-8950 #IN-REVIEW #comment add login button to menu & save form"

--- components/answers/SaveForm/SaveButton/index.tsx
+++ components/answers/SaveForm/SaveButton/index.tsx
@@ -1,15 +1,19 @@
+"use client";
+
 import { FunctionComponent } from "react";
 import { useFormStatus } from "react-dom";
 import { useTranslation } from "react-i18next";
 import IconComposer from "@rubin-epo/epo-react-lib/IconComposer";
 import * as Styled from "./styles";
 
-const SaveButton: FunctionComponent = () => {
+const SaveButton: FunctionComponent<{ isDisabled: boolean }> = ({
+  isDisabled = false,
+}) => {
   const { pending } = useFormStatus();
   const { t } = useTranslation();
 
   return (
-    <Styled.SaveButton disabled={pending} type="submit">
+    <Styled.SaveButton disabled={isDisabled || pending} type="submit">
       <Styled.SaveIcon>
         <IconComposer icon="FloppyDisk" size="1.5em" />
       </Styled.SaveIcon>
