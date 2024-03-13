daf9516ef2b871d30a31d0b67a040149a9d469a2
Alexandra Goff <agoff@lsst.org>
2024-03-13 10:49:47 -0700

    [C] extract inputs from equations

--- components/questions/Calculator/styles.ts
+++ components/questions/Calculator/styles.ts
@@ -1,9 +1,18 @@
 "use client";
 import styled from "styled-components";
+import { token } from "@rubin-epo/epo-react-lib/styles";
 
 export const CalculatorWrapper = styled.div`
   display: flex;
   justify-content: center;
-  font-size: 1.5em;
+  font-size: 100%;
   color: var(--neutral95, #1f2121);
+
+  @container (min-width: ${token("BREAK_MOBILE_MIN")}) {
+    font-size: 125%;
+  }
+
+  @container (min-width: ${token("BREAK_TABLET_MIN")}) {
+    font-size: 150%;
+  }
 `;
