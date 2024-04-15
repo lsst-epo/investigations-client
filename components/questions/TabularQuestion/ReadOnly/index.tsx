d1df86d50c3cdf20a1010a83ff42de7695a08fd1
Alexandra Goff <agoff@lsst.org>
2024-04-15 13:05:28 -0700

    [C] select cell use readonly display

--- components/questions/TabularQuestion/ReadOnly/index.tsx
+++ components/questions/TabularQuestion/ReadOnly/index.tsx
@@ -2,22 +2,46 @@
 
 import { FunctionComponent } from "react";
 import isObject from "lodash/isObject";
-import type { NumberInput, TextInput, WidgetInput } from "@/types/answers";
+import type {
+  NumberInput,
+  SelectInput,
+  TextInput,
+  WidgetInput,
+} from "@/types/answers";
 import useAnswer from "@/hooks/useAnswer";
 import * as Styled from "./styles";
+import { Option } from "@/components/shapes/option";
+import { getLabelByValue } from "@/components/questions/utils";
 
-const ReadOnlyCell: FunctionComponent<{ questionId: string; id: string }> = ({
+interface ReadOnlyCellProps {
+  questionId: string;
+  id: string;
+  options?: Array<Option>;
+}
+
+const ReadOnlyCell: FunctionComponent<ReadOnlyCellProps> = ({
   questionId,
+  options,
   id,
 }) => {
-  const { answer } = useAnswer<WidgetInput | TextInput | NumberInput>(
-    questionId
-  );
+  const { answer } = useAnswer<
+    WidgetInput | TextInput | NumberInput | SelectInput
+  >(questionId);
   const isTabularChild = isObject(answer);
 
   const cellValue = isTabularChild ? answer[id] : answer;
 
-  return <Styled.ReadOnly type="text" defaultValue={cellValue} readOnly />;
+  return (
+    <Styled.ReadOnly
+      type="text"
+      defaultValue={
+        options && options.length > 0
+          ? getLabelByValue(options, cellValue)
+          : cellValue
+      }
+      readOnly
+    />
+  );
 };
 
 export default ReadOnlyCell;
