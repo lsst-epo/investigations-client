5a9fdc55c5f270f994a43a492ecc1db487f79d5f
Alexandra Goff <agoff@lsst.org>
2024-04-16 14:56:02 -0700

    [C] source selector fixes

--- lib/api/fetcher.ts
+++ lib/api/fetcher.ts
@@ -1,3 +1,4 @@
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
+const fetcher = <T = any>(url: string): Promise<T> =>
+  fetch(url).then((response) => response.json());
 
 export default fetcher;
