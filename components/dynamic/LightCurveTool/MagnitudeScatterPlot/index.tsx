341bd4bf928770e81b422b431cfb2815416a3054
Alexandra Goff <agoff@lsst.org>
2024-04-16 14:56:02 -0700

    [C] source selector fixes

--- components/dynamic/LightCurveTool/MagnitudeScatterPlot/index.tsx
+++ components/dynamic/LightCurveTool/MagnitudeScatterPlot/index.tsx
@@ -1,9 +1,10 @@
 import { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
 import { ObservationsPlot } from "@rubin-epo/epo-widget-lib/LightCurvePlot";
-import useAlerts from "@/lib/api/hooks/useAlerts";
+import { Alert } from "@/lib/api/hooks/useAlerts";
 
 interface MagnitudeScatterPlotProps {
+  alerts: Array<Alert>;
   activeAlertIndex?: number;
   peakMjd: number;
   url?: string;
@@ -14,10 +15,15 @@ interface MagnitudeScatterPlotProps {
 
 const MagnitudeScatterPlotContainer: FunctionComponent<
   MagnitudeScatterPlotProps
-> = ({ activeAlertIndex, peakMjd, yMin, yMax, url, showPlot = true }) => {
+> = ({
+  activeAlertIndex,
+  peakMjd,
+  yMin,
+  yMax,
+  alerts = [],
+  showPlot = true,
+}) => {
   const { t } = useTranslation();
-
-  const { data: alerts = [], error, isLoading } = useAlerts(url);
 
   const activeAlertId =
     typeof activeAlertIndex === "number"
@@ -27,8 +33,10 @@ const MagnitudeScatterPlotContainer: FunctionComponent<
   return (
     <ObservationsPlot
       name={t("widgets.light_curve.title") || undefined}
-      alerts={showPlot && !isLoading && !error ? alerts : []}
-      {...{ peakMjd, yMin, yMax, activeAlertId }}
+      alerts={showPlot ? alerts : []}
+      yMin={yMin || undefined}
+      yMax={yMax || undefined}
+      {...{ peakMjd, activeAlertId }}
     />
   );
 };
