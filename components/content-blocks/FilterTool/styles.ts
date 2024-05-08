7f23d6854a11c223f72ee2bfb6be14c39f9be9c1
Alexandra Goff <agoff@lsst.org>
2024-05-08 10:51:35 -0700

    [C] round of tweaks

--- components/content-blocks/FilterTool/styles.ts
+++ components/content-blocks/FilterTool/styles.ts
@@ -1,5 +1,10 @@
 "use client";
+import WidgetContainerWithModal from "@/components/layout/WidgetContainerWithModal";
 import styled from "styled-components";
+
+export const WidgetContainer = styled(WidgetContainerWithModal)`
+  --widget-padding: 0px;
+`;
 
 export const WidgetInstructions = styled.div`
   color: var(--text-color);
