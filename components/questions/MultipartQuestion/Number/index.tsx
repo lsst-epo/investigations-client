84ddf59637509ffcc0ca4cb2fe66afcbc49b8330
Alexandra Goff <agoff@lsst.org>
2024-05-10 15:28:53 -0700

    [C] debounce number validators

--- components/questions/MultipartQuestion/Number/index.tsx
+++ components/questions/MultipartQuestion/Number/index.tsx
@@ -1,12 +1,18 @@
 "use client";
-import { FocusEventHandler, FunctionComponent } from "react";
+import {
+  FocusEventHandler,
+  FunctionComponent,
+  useEffect,
+  useState,
+} from "react";
+import toast from "react-hot-toast";
 import { graphql, useFragment, FragmentType } from "@/gql/public-schema";
 import { PartProps } from "..";
 import { stepFromPrecision } from "@/components/questions/utils";
-import { validateQuestion } from "@/components/questions/actions";
-import toast from "react-hot-toast";
 import useAnswer from "@/hooks/useAnswer";
 import { MultipartQuestionData } from "@/types/answers";
+import { useDebounce } from "@/hooks/useDebounce";
+import { validateQuestion } from "../../actions";
 import * as Styled from "../Text/styles";
 
 const Fragment = graphql(`
@@ -33,27 +39,46 @@ const NumberPart: FunctionComponent<
   );
   const { answer = {}, onChangeCallback } =
     useAnswer<MultipartQuestionData>(questionId);
+  const [value, setValue] = useState<number>(answer[id] as number);
+  const debouncedValue = useDebounce(value, 500);
+
+  const validate = async () => {
+    const result = await validateQuestion({
+      id: questionId,
+      partId: id,
+      value: debouncedValue,
+      locale,
+    });
+
+    if (result) {
+      toast.dismiss();
+      toast(result);
+    }
+  };
+
+  const handleBlur: FocusEventHandler<HTMLInputElement> = (event) => {
+    const value = parseFloat(event.target.value);
+
+    if (value !== debouncedValue) {
+      validate();
+    }
+
+    onChangeCallback && onChangeCallback({ ...answer, [id]: value });
+  };
 
   const handleChange: FocusEventHandler<HTMLInputElement> = async (event) => {
     const value = parseFloat(event.target.value);
 
     if (value !== answer[id]) {
-      if (validation && validation.length > 0) {
-        const result = await validateQuestion({
-          id: questionId,
-          partId: id,
-          value,
-          locale,
-        });
-
-        if (result) {
-          toast(result);
-        }
-      }
-
-      onChangeCallback && onChangeCallback({ ...answer, [id]: value });
+      setValue(value);
     }
   };
+
+  useEffect(() => {
+    if (debouncedValue !== answer[id] && validation && validation.length > 0) {
+      validate();
+    }
+  }, [debouncedValue]);
 
   const step = stepFromPrecision(precision);
 
@@ -63,7 +88,8 @@ const NumberPart: FunctionComponent<
       min={minimum}
       max={maximum}
       defaultValue={answer[id]}
-      onBlur={handleChange}
+      onBlur={handleBlur}
+      onChange={handleChange}
       {...{ id, step }}
     />
   );
