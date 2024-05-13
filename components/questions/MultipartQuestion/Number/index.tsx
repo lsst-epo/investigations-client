e28216fb5b432440a6ea481dea162ee7b1bf0028
Alexandra Goff <agoff@lsst.org>
2024-05-13 11:58:10 -0700

    [C] finish debounce

--- components/questions/MultipartQuestion/Number/index.tsx
+++ components/questions/MultipartQuestion/Number/index.tsx
@@ -1,17 +1,12 @@
 "use client";
-import {
-  FocusEventHandler,
-  FunctionComponent,
-  useEffect,
-  useState,
-} from "react";
+import { FocusEventHandler, FunctionComponent } from "react";
 import toast from "react-hot-toast";
 import { graphql, useFragment, FragmentType } from "@/gql/public-schema";
+import { useDebouncedCallback } from "use-debounce";
 import { PartProps } from "..";
 import { stepFromPrecision } from "@/components/questions/utils";
 import useAnswer from "@/hooks/useAnswer";
 import { MultipartQuestionData } from "@/types/answers";
-import { useDebounce } from "@/hooks/useDebounce";
 import { validateQuestion } from "../../actions";
 import * as Styled from "../Text/styles";
 
@@ -39,14 +34,14 @@ const NumberPart: FunctionComponent<
   );
   const { answer = {}, onChangeCallback } =
     useAnswer<MultipartQuestionData>(questionId);
-  const [value, setValue] = useState<number>(answer[id] as number);
-  const debouncedValue = useDebounce(value, 500);
+  const step = stepFromPrecision(precision);
+  const hasValidation = validation && validation.length > 0;
 
-  const validate = async () => {
+  const validate = async (value?: number) => {
     const result = await validateQuestion({
       id: questionId,
       partId: id,
-      value: debouncedValue,
+      value,
       locale,
     });
 
@@ -56,40 +51,24 @@ const NumberPart: FunctionComponent<
     }
   };
 
-  const handleBlur: FocusEventHandler<HTMLInputElement> = (event) => {
-    const value = parseFloat(event.target.value);
-
-    if (value !== debouncedValue) {
-      validate();
-    }
-
-    onChangeCallback && onChangeCallback({ ...answer, [id]: value });
-  };
-
   const handleChange: FocusEventHandler<HTMLInputElement> = async (event) => {
     const value = parseFloat(event.target.value);
 
     if (value !== answer[id]) {
-      setValue(value);
+      hasValidation && validate(value);
+      onChangeCallback && onChangeCallback({ ...answer, [id]: value });
     }
   };
 
-  useEffect(() => {
-    if (debouncedValue !== answer[id] && validation && validation.length > 0) {
-      validate();
-    }
-  }, [debouncedValue]);
-
-  const step = stepFromPrecision(precision);
+  const debouncedHandleChange = useDebouncedCallback(handleChange, 500);
 
   return (
     <Styled.InlineTextInput
       type="number"
       min={minimum}
       max={maximum}
-      defaultValue={answer[id]}
-      onBlur={handleBlur}
-      onChange={handleChange}
+      defaultValue={answer[id] || undefined}
+      onChange={debouncedHandleChange}
       {...{ id, step }}
     />
   );
