b31ec31d434a310b11ceedc959ba1832e7b8a46d
Alexandra Goff <agoff@lsst.org>
2024-05-14 16:54:15 -0700

    [C] map source selector to percent

--- helpers/widgets.ts
+++ helpers/widgets.ts
@@ -4,7 +4,7 @@ export const combineAlertsAndImages = (
   alerts: Array<Alert | any>,
   images: Array<any>
 ) => {
-  const size = 1000;
+  const size = 200;
 
   return {
     alerts: alerts.map((alert, i) => {
@@ -12,18 +12,28 @@ export const combineAlertsAndImages = (
         url: { directUrlPreview },
       } = images[i];
 
-      const urlWithoutConstraint = directUrlPreview.slice(0, -3);
-
       return {
         ...alert,
         ...alert,
         image: {
           width: size,
           height: size,
-          url: `${urlWithoutConstraint}${size}`,
+          url: directUrlPreview,
         },
       };
     }),
     size,
   };
 };
+
+export const percentageMapSources = <T extends { x: any; y: any; radius: any }>(
+  sources: Array<T>
+) =>
+  sources.map(({ x, y, radius, ...source }) => {
+    return {
+      x: `${x}%`,
+      y: `${y}%`,
+      radius: `${radius}%`,
+      ...source,
+    };
+  });
