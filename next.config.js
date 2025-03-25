9214db2da241a2bb0f2732382d886576ca51179c
Alexandra Moskowitz <agoff@lsst.org>
2025-03-25 10:54:31 -0400

    build: port from CommonJS to ESM

--- next.config.js
+++ next.config.js
@@ -1,37 +1,19 @@
-const path = require("path");
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
-let API_URL;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// Check to see if the environment variable DOCKER_GATEWAY_IP is populated, if so
-// then the URL should be constructed for a Docker static build
-if (
-  process.env.DOCKERIZED &&
-  process.env.DOCKER_GATEWAY_IP &&
-  parseInt(process.env.DOCKER_GATEWAY_IP) !== -1 && // The getApiGatewayURL script returns -1 if an error occurs grabbing the IP
-  process.env.DOCKER_GATEWAY_PORT
-) {
-  API_URL = `http://${process.env.DOCKER_GATEWAY_IP}:${process.env.DOCKER_GATEWAY_PORT}`;
-}
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+export default {
   async generateBuildId() {
     return "investigations-client-next-build-id";
   },
-  async rewrites() {
-    return [
-      {
-        source: "/assets/:path*",
-        destination: API_URL
-          ? `${API_URL}/assets/:path*`
-          : "http://localhost:9000/assets/:path*", // Proxy to Backend
-      },
-    ];
-  },
   experimental: {
-    isrMemoryCacheSize: 0,
     forceSwcTransforms: true,
     optimizePackageImports: ["@rubin-epo/epo-react-lib"],
   },
+  cacheMaxMemorySize: 0,
   swcMinify: true,
   compiler: {
     // Enables the styled-components SWC transform
@@ -42,8 +24,8 @@ module.exports = {
   },
   sassOptions: {
     includePaths: [
-      path.join(__dirname, "theme/styles"),
-      path.join(__dirname, "components"),
+      join(__dirname, "theme/styles"),
+      join(__dirname, "components"),
     ],
   },
   webpack(config) {
