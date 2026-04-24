3fc15c4119ccaf8ff16f3dbc8d005aef7b3dc7ac
Jeff Innes <jeff.innes@noirlab.edu>
2026-04-23 17:51:52 -0700

    feat: add SelectionList component to OrbitalSim question widget
    define selectedAnswer state management and add styled component wrapper for the SelectionList

--- components/content-blocks/OrbitalSim/styles.ts
+++ components/content-blocks/OrbitalSim/styles.ts
@@ -4,3 +4,12 @@ export const OrbitalSimWidgetContainer = styled.div`
     min-height: 500px;
     width: 100%;
 `;
+
+export const SelectionListWrapper = styled.div`
+    max-width: 30%;
+    margin-top: var(--PADDING_SMALL);
+    margin-bottom: var(--PADDING_SMALL);
+    button {
+        padding: 0px;
+    }
+`;
