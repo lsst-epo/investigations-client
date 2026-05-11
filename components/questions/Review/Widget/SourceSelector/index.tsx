b7bad30fdfbe985c4de80b48e95d158f48008a55
Jeff Innes <jeff.innes@noirlab.edu>
2026-05-11 12:01:54 -0700

    fix: update `SourceSelector` review widget to handle the `MovingSourceSelector` widget
    bump @rubin-epo/epo-react-lib to v3.0.2 and @rubin-epo/epo-widget-lib to v2.0.10
    This fix depends on a fix applied in the update to those packages.

--- components/questions/Review/Widget/SourceSelector/index.tsx
+++ components/questions/Review/Widget/SourceSelector/index.tsx
@@ -1,11 +1,15 @@
 import { FunctionComponent } from "react";
-import SourceSelector from "@rubin-epo/epo-widget-lib/SourceSelector";
+import SourceSelector, {
+  MovingSourceSelector,
+} from "@rubin-epo/epo-widget-lib/SourceSelector";
 import { WidgetReviewProps } from "..";
 import { SourceSelectorData } from "@/types/widgets";
 import useAlerts from "@/lib/api/hooks/useAlerts";
 import {
   combineAlertsAndImages,
   percentageMapSources,
+  percentageMapSourcesForMovingSources,
+  combineAlertsAndImagesForMovingSources,
 } from "@/helpers/widgets";
 
 const SourceSelectorReview: FunctionComponent<
@@ -16,20 +20,38 @@ const SourceSelectorReview: FunctionComponent<
   const [{ sources, json, imageAlbum }] = dataset;
   const { selectedSource = [] } = value || {};
 
+  const hasMovingSource = sourceSelector[0]?.hasMovingSource ?? false;
+
   const { data: alertData = [], isLoading } = useAlerts(json[0].url);
 
-  const { alerts, size } = combineAlertsAndImages(alertData, imageAlbum || []);
+  const { alerts, size } = hasMovingSource
+    ? combineAlertsAndImagesForMovingSources(alertData, imageAlbum || [])
+    : combineAlertsAndImages(alertData, imageAlbum || []);
 
-  const percentageMappedSources = percentageMapSources(sources);
+  const percentageMappedSources = hasMovingSource
+    ? percentageMapSourcesForMovingSources(sources)
+    : percentageMapSources(sources);
 
   return (
-    <SourceSelector
-      {...{ alerts, selectedSource, isLoading }}
-      sources={percentageMappedSources}
-      width={size}
-      height={size}
-      isDisplayOnly
-    />
+    <>
+      {hasMovingSource ? (
+        <MovingSourceSelector
+          {...{ alerts, selectedSource, isLoading }}
+          movingSources={percentageMappedSources}
+          width={size}
+          height={size}
+          isDisplayOnly
+        />
+      ) : (
+        <SourceSelector
+          {...{ alerts, selectedSource, isLoading }}
+          sources={percentageMappedSources}
+          width={size}
+          height={size}
+          isDisplayOnly
+        />
+      )}
+    </>
   );
 };
 
