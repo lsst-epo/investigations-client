acbc90ab356ecce1702cbab047c7291e44786556
Sasha Persaud <sasharavpersaud@gmail.com>
2026-05-13 13:57:03 -0700

    fix: fixed OrbitalSim modal and fullscreen styling

--- components/questions/Widget/OrbitalSim/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import WidgetContainerWithModal from "@/components/layout/WidgetContainerWithModal";
-
-export const WidgetContainer = styled(WidgetContainerWithModal)`
-    width: 100% !important;
-`;
