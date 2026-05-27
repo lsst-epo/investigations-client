b65bc8db00c7a61f0e5cd786bafb5c4df072ffba
Jeff Innes <jeff.innes@noirlab.edu>
2026-05-27 13:58:20 -0700

    feat: update the OrbitalSimWidget content-block to handle random assignment of NEO on first visit
    and persisting the assignment via local storage, but only for OrbitalSim widgets
    where the Details Table should be shown.

--- components/content-blocks/OrbitalSim/OrbitalSimWidget.tsx
+++ components/content-blocks/OrbitalSim/OrbitalSimWidget.tsx
@@ -1,6 +1,18 @@
-import { FunctionComponent, use } from "react";
-import { OrbitalSimProvider, OrbitalSim } from "@rubin-epo/epo-widget-lib/OrbitalSim";
+"use client";
+
+import type { WidgetInput } from "@/types/answers";
+
+import { FunctionComponent, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  OrbitalSimProvider,
+  OrbitalSim,
+} from "@rubin-epo/epo-widget-lib/OrbitalSim";
+import SelectListbox from "@rubin-epo/epo-react-lib/SelectListbox";
 import { BaseContentBlockProps } from "@/components/shapes";
+import useAnswer from "@/hooks/useAnswer";
+import Loader from "@/components/page/Loader";
+import { isUserAnEducator } from "@/components/auth/serverActions";
 import { graphql, useFragment, FragmentType } from "@/gql/public-schema";
 import * as Styled from "./styles";
 
@@ -8,69 +20,138 @@ const Fragment = graphql(`
   fragment OrbitalSimWidget on contentBlocks_orbitalSimWidget_BlockType {
     __typename
     orbitalSimTool {
-        ... on widgets_orbitalSim_Entry {
+      id
+      ... on widgets_orbitalSim_Entry {
         addTimeControls
         allowOrbitRotation
         showDetailsTable
         containsSwappableOrbits
         orbitalDatasets {
-            ... on orbitalDatasets_orbital_BlockType {
-                orbitalSimData {
-                ... on datasets_orbitalSimOrbits_Entry {
-                    json {
-                        url
-                    }
+          ... on orbitalDatasets_orbital_BlockType {
+            orbitalSimData {
+              ... on datasets_orbitalSimOrbits_Entry {
+                json {
+                  url
                 }
+              }
             }
+          }
         }
-        }
-    }
+      }
     }
   }
 `);
 
-const OrbitalSimWidget: FunctionComponent<BaseContentBlockProps<FragmentType<typeof Fragment>>> = ({ data, locale }) => {
-
-    /**
-     * Add typing for `orbitalSimTool, this may come from codegen type
-     */
-    const { orbitalSimTool } = useFragment(Fragment, data);
-
-    /**
-     * to-do: use a helper like @/helpers/widgets::getDataset() to extract the URL
-     *        or create a new one
-     */
-    const url = orbitalSimTool[0]?.orbitalDatasets[0]?.orbitalSimData[0]?.json[0]?.url;
-    const addTimeControls = orbitalSimTool[0]?.addTimeControls;
-    const allowOrbitRotation = orbitalSimTool[0]?.allowOrbitRotation;
-    const showDetailsTable = orbitalSimTool[0]?.showDetailsTable;
-    const swappableOrbits = orbitalSimTool[0]?.containsSwappableOrbits;
-
-    const dataObj = use(getOrbitalData(url));
-
-    /**
-     * to-do: flesh out styling for `OrbitalSimWidgetContainer`
-     */
-    return (
-        <Styled.OrbitalSimWidgetContainer>
-            <OrbitalSimProvider swappableOrbits={swappableOrbits} orbitData={ dataObj.orbits } showDetailsTable={showDetailsTable} allowOrbitRotation={allowOrbitRotation} showTimeControls={addTimeControls}>
-                <OrbitalSim/>
-            </OrbitalSimProvider>
-        </Styled.OrbitalSimWidgetContainer>
-    )
+function getRandomIndex(max: number) {
+  return Math.floor(Math.random() * max);
 }
 
-/**
- * to-do: use `fetcher` from `/lib/api` instead
- */
-async function getOrbitalData(url:string) {
+const OrbitalSimWidget: FunctionComponent<
+  BaseContentBlockProps<FragmentType<typeof Fragment>>
+> = ({ data, locale }) => {
+  const [dataObj, setDataObj] = useState(null);
+  const [url, setUrl] = useState<string>("");
+  const [isEducator, setIsEducator] = useState(false);
+  const { t } = useTranslation();
 
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error("A fetch error occurred!");
+  /**
+   * Add typing for `orbitalSimTool, this may come from codegen type
+   */
+  const { orbitalSimTool } = useFragment(Fragment, data);
+  const { answer: value, onChangeCallback: setNeoIndex } =
+    useAnswer<WidgetInput>(orbitalSimTool[0]?.id || "");
+
+  const assignedNeoIndex = value?.assignedNeoIndex;
+  const addTimeControls = orbitalSimTool[0]?.addTimeControls;
+  const allowOrbitRotation = orbitalSimTool[0]?.allowOrbitRotation;
+  const showDetailsTable = orbitalSimTool[0]?.showDetailsTable;
+  const swappableOrbits = orbitalSimTool[0]?.containsSwappableOrbits;
+
+  if (url === "") {
+    /**
+     * to-do: use a helper like @/helpers/widgets::getDataset() to extract  the URL
+     * or create a new one
+     */
+    setUrl(
+      orbitalSimTool[0]?.orbitalDatasets[0]?.orbitalSimData[0]?.json[0]?.url,
+    );
+  }
+
+  useEffect(() => {
+    async function callServerFunction() {
+      setIsEducator(await isUserAnEducator());
     }
-    const dataObj = await response.json();
-    return dataObj;
-}
+    callServerFunction();
+  }, []);
+
+  useEffect(() => {
+    if (!showDetailsTable || !dataObj || assignedNeoIndex != null) return;
+
+    const newIndex = getRandomIndex(dataObj?.orbits?.neos?.length ?? 0);
+    setNeoIndex({
+      assignedNeoIndex: newIndex,
+      dataType: "content",
+    });
+  }, [showDetailsTable, dataObj, assignedNeoIndex, setNeoIndex]);
+
+  useEffect(() => {
+    async function getOrbitalData(url: string) {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Error fetching the JSON dataset for the Orbital Sim!");
+      }
+      const res = await response.json();
+      setDataObj(res);
+    }
+
+    if (dataObj === null) {
+      getOrbitalData(url);
+    }
+  }, [url, dataObj]);
+
+  /**
+   * to-do: flesh out styling for `OrbitalSimWidgetContainer`
+   */
+  return (
+    <Styled.OrbitalSimWidgetContainer>
+      {dataObj === null ? (
+        <Loader />
+      ) : (
+        <>
+          <OrbitalSimProvider
+            swappableOrbits={swappableOrbits}
+            orbitData={dataObj.orbits}
+            showDetailsTable={showDetailsTable}
+            allowOrbitRotation={allowOrbitRotation}
+            showTimeControls={addTimeControls}
+            selectedNeoIndex={
+              showDetailsTable ? assignedNeoIndex ?? 0 : undefined
+            }>
+            <OrbitalSim />
+          </OrbitalSimProvider>
+          {showDetailsTable && isEducator && (
+            <>
+              <p>{t("widgets.orbital_sim.instructor_neo_override_tip")}</p>
+              <SelectListbox
+                value={assignedNeoIndex}
+                options={dataObj?.orbits?.neos?.map((neo, i) => ({
+                  value: i,
+                  label: neo.Principal_desig,
+                }))}
+                onChangeCallback={(newIndex) =>
+                  setNeoIndex({
+                    ...value,
+                    assignedNeoIndex: newIndex,
+                    dataType: "content",
+                  })
+                }
+              />
+            </>
+          )}
+        </>
+      )}
+    </Styled.OrbitalSimWidgetContainer>
+  );
+};
 
 export default OrbitalSimWidget;
